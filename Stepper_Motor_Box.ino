07ead6abc527f2597cf594b4ccb8e179239f5142
Jon MacDonald <jonmacd@gmail.com>
2021-11-08 16:42:46 -0400

    commented out disabling the motor once stopped

--- Stepper_Motor_Box.ino
+++ Stepper_Motor_Box.ino
@@ -246,7 +246,7 @@ void stopMotor(){
   // STOP MOTOR HERE
   matrix.println("stop");
   matrix.writeDisplay();
-  digitalWrite(enPin, HIGH);
+  // digitalWrite(enPin, HIGH);
   digitalWrite(stLED, HIGH);
   delay(500);
   lastDnState = false;
